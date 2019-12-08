27015a19304c2b5cde8e25e766def61f61b6d2ef
Katie McBride <mcbride.katieb@gmail.com>
2019-12-07 22:59:18 -0600

    add department

--- cms.js
+++ cms.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -71,3 +72,53 @@ function start() {
         }
       });
   }
+
+  function viewDepartment() {
+    inquirer
+      .prompt({
+        name: "department",
+        type: "list",
+        message: "What department would you like to see?",
+        choices: [
+            "Sales",
+            "Legal",
+            "Engineer",
+            "Finance"
+          ]
+      })
+      .then(function(answer) {
+        console.log(answer.name);
+        connection.query("SELECT * FROM department WHERE ?", { department_name: answer.department }, function(err, res) {
+          console.log(
+            "Department Name: " +
+              res[0].department_name +
+              " || Id " +
+              res[0].id
+            )
+        //   runSearch();
+        });
+      });
+  }
+
+
+//   ADD DEPARTMENT
+function addDepartment() {
+    inquirer
+      .prompt({
+        name: "addDep",
+        type: "input",
+        message: "What department would you like to add?",
+      })
+      .then(answer =>{
+        connection.query(
+            'INSERT INTO department SET ?',
+            {
+                department_name: answer.addDep,
+            },
+            err => {
+                if (err) throw err;
+                console.log('your department was created')
+            }
+        );
+    })
+}
