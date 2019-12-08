4c3c07b4a60bc426826df7ef637ba55c9cb0a3c0
Katie McBride <mcbride.katieb@gmail.com>
2019-12-07 21:10:10 -0600

    sql database

---
+++ cms.js
@@ -0,0 +1,73 @@
+var mysql = require("mysql");
+const inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Ktbuh165!",
+  database: "cms_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  start();
+});
+
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "View department",
+          "View role",
+          "View employee",
+          "Update Employee Role"
+
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add a department":
+          addDepartment();
+          break;
+  
+        case "Add a role":
+          addRole();
+          break;
+  
+        case "Add an employee":
+          addEmployee();
+          break;
+  
+        case "View department":
+          viewDepartment();
+          break;
+  
+        case "View role":
+          viewRole();
+          break;
+
+        case "View employee":
+          viewEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+        }
+      });
+  }
---
+++ seed.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE DATABASE role_db;
+
+USE role_db;
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4) NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+)
